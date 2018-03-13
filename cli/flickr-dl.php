de0022600625acc322ade7d5cca20f3269cc3781
Surya Oktafendri <f2face@f2face.com>
2018-03-13 09:42:56 +0700

    Initial commit

---
+++ cli/flickr-dl.php
@@ -0,0 +1,16 @@
+<?php
+// Autoload
+require_once(__DIR__.'/../vendor/autoload.php');
+
+use f2face\FlickrDL\FlickrDL;
+
+// Photo save path
+define('SAVE_PATH', __DIR__. '/downloads');
+
+$flickr_dl = new FlickrDL();
+$result = $flickr_dl->getBest($argv[1]);
+
+$f = fopen(rtrim(SAVE_PATH, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . basename($result->source), 'w');
+fwrite($f, file_get_contents($result->source));
+fclose($f);
+?>
---
+++ src/FlickrDL.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ *  @file FlickrDL.php
+ *  @brief Download the largest size of Flickr photo
+ */
+namespace f2face\FlickrDL;
+
+class FlickrDL
+{
+    protected $api_key;
+    
+    private $api_endpoint = 'https://www.flickr.com/services/api/render?method=flickr.photos.getSizes&api_key=%s&photo_id=%s&format=json&nojsoncallback=1';
+    
+    public function __construct($api_key = null)
+    {
+        $this->api_key = is_string($api_key) ? $api_key : '39bf0d19f3415af572548735d6b817e1';
+    }
+    
+    public function setApiKey(string $api_key)
+    {
+        $this->api_key = $api_key;
+        return $this;
+    }
+    
+    public function getApiKey()
+    {
+        return $this->api_key;
+    }
+    
+    public function getBest(string $url)
+    {
+        $api = sprintf($this->api_endpoint, $this->api_key, $this->getFileIdFromUrl($url));
+        
+        $obj = $this->getResourceAndSanitizeData($api);
+        
+        if (empty($obj->sizes->size))
+            throw new \Exception('Photo not found', 404);
+        
+        return end($obj->sizes->size);
+    }
+    
+    protected function getFileIdFromUrl(string $url)
+    {
+        if (!$this->isValidFlickrUrl($url))
+            throw new \Exception('URL is not valid.', 404);
+        
+        preg_match('#/(\d+)/?#', $url, $match);
+        return $match[1];
+    }
+    
+    protected function isValidFlickrUrl(string $url)
+    {
+        return (bool) preg_match('#^https?://([w\-]+\.)?flickr.com/photos/[^/]+/\d+#i', $url);
+    }
+    
+    protected function getResourceAndSanitizeData(string $api_url)
+    {
+        $data = NULL;
+        
+        if (function_exists('file_get_contents'))
+            $data = file_get_contents($api_url);
+        
+        preg_match(
+            '#<pre>(.+)</pre>#',
+            preg_replace('#(\r|\n)#', '', $data),
+            $match
+        );
+        
+        $obj = json_decode($match[1]);
+        
+        if (!$obj)
+            throw new \Exception('Error parsing data.', 500);
+        
+        return $obj;
+    }
+}
