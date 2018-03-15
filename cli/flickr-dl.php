71e6ff7ddbe7827336ac44e9e531cc3731423df6
Surya Oktafendri <f2face@f2face.com>
2018-03-15 19:00:41 +0000

    Apply fixes from StyleCI

--- cli/flickr-dl.php
+++ cli/flickr-dl.php
@@ -1,17 +1,17 @@
 <?php
+
 // Autoload
-require_once(__DIR__.'/../vendor/autoload.php');
+require_once __DIR__.'/../vendor/autoload.php';
 
 use f2face\FlickrDL\FlickrDL;
 
 // Photo save path
-define('SAVE_PATH', __DIR__. '/downloads');
+define('SAVE_PATH', __DIR__.'/downloads');
 
 $flickr_dl = new FlickrDL();
 $flickr_dl->refreshApiKey();
 $result = $flickr_dl->getBest($argv[1]);
 
-$f = fopen(rtrim(SAVE_PATH, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . basename($result->source), 'w');
+$f = fopen(rtrim(SAVE_PATH, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.basename($result->source), 'w');
 fwrite($f, file_get_contents($result->source));
 fclose($f);
-?>
--- src/FlickrDL.php
+++ src/FlickrDL.php
@@ -3,93 +3,100 @@
  *  @file FlickrDL.php
  *  Get the largest photo from Flickr URL.
  */
+
 namespace f2face\FlickrDL;
 
 class FlickrDL
 {
     protected $api_key;
-    
+
     private $api_endpoint = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&format=json&nojsoncallback=1&api_key=%s&photo_id=%s';
-    
+
     public function __construct($api_key = null)
     {
         $this->api_key = $api_key;
     }
-    
+
     public function setApiKey(string $api_key)
     {
         $this->api_key = $api_key;
+
         return $this;
     }
-    
+
     public function getApiKey()
     {
         return $this->api_key;
     }
-    
+
     public function getBest(string $url)
     {
         $sizes = $this->retrieveSizes($url);
+
         return end($sizes);
     }
-    
+
     public function getSquare(string $url)
     {
         $sizes = $this->retrieveSizes($url);
+
         return $sizes[0];
     }
-    
+
     public function refreshApiKey()
     {
         // Get the latest API key from API Explorer
         $flickr_api_explorer = 'https://www.flickr.com/services/api/explore/flickr.photos.getSizes';
         $data = file_get_contents($flickr_api_explorer);
         preg_match('#"api_key":"(\w+)"#i', $data, $match);
-        
-        if (empty($match[1]))
+
+        if (empty($match[1])) {
             throw new \Exception('Failed refreshing the API key.');
-        
+        }
         $this->api_key = $match[1];
+
         return $match[1];
     }
-    
+
     protected function retrieveSizes(string $url)
     {
         $api = sprintf($this->api_endpoint, $this->api_key, $this->getFileIdFromUrl($url));
-        
+
         $obj = $this->getResourceAndSanitizeData($api);
-        
-        if (empty($obj->sizes->size))
+
+        if (empty($obj->sizes->size)) {
             throw new \Exception('Photo not found', 404);
-        
+        }
         return $obj->sizes->size;
     }
-    
+
     protected function getFileIdFromUrl(string $url)
     {
-        if (!$this->isValidFlickrUrl($url))
+        if (!$this->isValidFlickrUrl($url)) {
             throw new \Exception('URL is not valid.', 404);
-        
+        }
         preg_match('#/(\d+)/?#', $url, $match);
+
         return $match[1];
     }
-    
+
     protected function isValidFlickrUrl(string $url)
     {
         return (bool) preg_match('#^https?://([w\-]+\.)?flickr.com/photos/[^/]+/\d+#i', $url);
     }
-    
+
     protected function getResourceAndSanitizeData(string $api_url)
     {
-        $data = NULL;
-        if (function_exists('file_get_contents'))
+        $data = null;
+        if (function_exists('file_get_contents')) {
             $data = file_get_contents($api_url);
-        
+        }
+
         $obj = json_decode($data);
-        
-        if (!$obj)
+
+        if (!$obj) {
             throw new \Exception('Error parsing data.', 500);
-        
+        }
         return $obj;
     }
 }
